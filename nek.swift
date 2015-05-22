71e440478a14dc768d9910d6d442f76e1c179e80
Max Hutchinson <maxhutch@gmail.com>
2015-05-22 09:11:02 -0500

    Two viscosities are actually executed in a loop!
    Be sure to run `git submodule update --remote`, as nek-tools and nek
    changed to suppor this script.

--- nek.swift
+++ nek.swift
@@ -3,18 +3,18 @@ type file;
 app 
 (file _out, file _err, file _nek5000, file _rea, file _map, file _config)
 app_genrun
-(file _json, file _tusr, string _makenek, string _name)
+(file _json, file _tusr, string _makenek, string _name, string _tdir)
 /*
 ~/maxhutch/nek-tools/genrun/genrun.py -d LST.json -u LST_f90.tusr --makenek ~/maxhutch/nek/makenek RTI_LST 
 */
 {
-  genrun "-d" @_json "-u" @_tusr "--makenek" _makenek _name stdout=@_out stderr=@_err;
+  genrun "-d" @_json "-u" @_tusr "--makenek" _makenek "--tdir" _tdir _name stdout=@_out stderr=@_err;
 }
 
 app
 (file _out, file _err, file[] _RTIfiles)
 app_donek
-(file _rea, file _map, file _nek5000, string _name)
+(file _rea, file _map, string _tdir, string _name, file _nek5000)
 /*
 requires two input files and an exe file (acts as input here) that are prefixed as RTI_LST
  RTI_LST.rea
@@ -23,7 +23,7 @@ requires two input files and an exe file (acts as input here) that are prefixed 
  ~/maxhutch/nek/nekmpi RTI_LST 1
 */
 {
- nekmpi _name "1" stdout=@_out stderr=@_err;
+ nekmpi _name "1" _tdir stdout=@_out stderr=@_err;
 }
 
 /*
@@ -50,36 +50,43 @@ app_make_config
 /* Inputs! */
 file json <"LST.json">;
 file tusr <"LST_f90.tusr">;
-float[] viscosities=[0.001];
+float[] viscosities=[0.001, 0.002];
 
 
 foreach viscosity,i in viscosities {
 
+string tdir = sprintf("./tmp%d", i);
 
 /* app invocation */
 
-file sed_e <"sed_err.txt">;
-file runconfig <"LST_sub.json">;
+file sed_e     <single_file_mapper; file=sprintf("%s/sed_err.txt", tdir)>;
+file runconfig <single_file_mapper; file=sprintf("%s/LST_sub.json", tdir)>;
 (sed_e, runconfig) = app_make_config(json, viscosity );
 
-file nek5000 <"nek5000">;
-file genrun_o <"genrun_out.txt">;
-file genrun_e <"genrun_err.txt">;
+file genrun_o <single_file_mapper; file=sprintf("%s/genrun_out.txt", tdir)>;
+file genrun_e <single_file_mapper; file=sprintf("%s/genrun_err.txt", tdir)>;
 string name=sprintf("RTI_LST-%f", viscosity);
-file RTIjson <single_file_mapper; file=sprintf("./%s.json", name)>;
-file rea     <single_file_mapper; file=sprintf("./%s.rea", name)>;
-file map     <single_file_mapper; file=sprintf("./%s.map", name)>;
+file RTIjson <single_file_mapper; file=sprintf("%s/%s.json", tdir, name)>;
+file rea     <single_file_mapper; file=sprintf("%s/%s.rea",  tdir, name)>;
+file map     <single_file_mapper; file=sprintf("%s/%s.map",  tdir, name)>;
+file nek5000 <single_file_mapper; file=sprintf("%s/nek5000", tdir, name)>;
 
-(genrun_o, genrun_e, nek5000, rea, map, RTIjson) = app_genrun (runconfig, tusr, "@pwd/nek/makenek", name);
+(genrun_o, genrun_e, nek5000, rea, map, RTIjson) = app_genrun (runconfig, tusr, "@pwd/nek/makenek", name, tdir);
 
-file donek_o <"donek_out.txt">;
-file donek_e <"donek_err.txt">;
-file[] RTIfiles <filesys_mapper; pattern=sprintf("%s0.f*",name)>;
-(donek_o, donek_e, RTIfiles) = app_donek(rea, map, nek5000, name);
+file donek_o <single_file_mapper; file=sprintf("%s/donek_out.txt", tdir)>;
+file donek_e <single_file_mapper; file=sprintf("%s/donek_err.txt", tdir)>;
 
-file analyze_o <"analyze_out.txt">;
-file analyze_e <"analyze_err.txt">;
-file[] pngs <filesys_mapper; pattern=sprintf("%s*.png", name)>;
-(analyze_o, analyze_e, pngs) = app_nek_analyze(RTIjson, RTIfiles, sprintf("./%s",name));
+string[auto] RTI_outfile_names;
+foreach j in [1:6] {
+  RTI_outfile_names << sprintf("%s/%s0.f0000%i", tdir, name, j);
+}
+
+file[] RTIfiles <array_mapper; files=RTI_outfile_names>;
+(donek_o, donek_e, RTIfiles) = app_donek(rea, map, tdir, name, nek5000);
+
+file analyze_o <single_file_mapper; file=sprintf("%s/analyze_out.txt", tdir)>;
+file analyze_e <single_file_mapper; file=sprintf("%s/analyze_err.txt", tdir)>;
+file[] pngs <filesys_mapper; pattern=sprintf("%s/%s*.png", tdir, name)>;
+(analyze_o, analyze_e, pngs) = app_nek_analyze(RTIjson, RTIfiles, sprintf("%s/%s",tdir,name));
 
 }
