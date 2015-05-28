4fec1f34c0a6eaad61e7df17949be2616ff466c2
Max Hutchinson <maxhutch@gmail.com>
2015-05-28 18:14:30 +0000

    Working example for ALCF machines (cetus and tukey).

--- apps.swift
+++ apps.swift
@@ -1,14 +1,22 @@
 type file;
 
 app 
-(file _nek5000, file _rea, file _map, file _config)
+(file _usr, file _rea, file _map, file _config, file _size)
 app_genrun
 (file _json, file _tusr, string _makenek, string _name, string _tdir)
 /*
 ~/maxhutch/nek-tools/genrun/genrun.py -d LST.json -u LST_f90.tusr --makenek ~/maxhutch/nek/makenek RTI_LST 
 */
 {
-  genrun "-d" @_json "-u" @_tusr "--makenek" _makenek "--tdir" _tdir _name ;
+  genrun "-d" @_json "-u" @_tusr "--makenek" _makenek "--tdir" _tdir _name "--no-make";
+}
+
+app
+(file _nek5000)
+app_makenek
+(string _tdir, string _name, file _usr, file _size)
+{
+  makenek "/home/maxhutch/simple/nek/makenek" _name "/home/maxhutch/simple/nek/" _tdir;
 }
 
 app
@@ -23,7 +31,7 @@ requires two input files and an exe file (acts as input here) that are prefixed 
  ~/maxhutch/nek/nekmpi RTI_LST 1
 */
 {
- nekmpi _name "1" _tdir stdout=@_out stderr=@_err;
+ nekmpi _name "1" "64" _tdir stdout=@_out stderr=@_err;
 }
 
 /*
@@ -36,7 +44,7 @@ app
 app_nek_analyze
 (file _RTIjson, file[] _RTIfiles, string _name)
 {
- nek_analyze _name "-f" "1" "-e" "6" stdout=@_out stderr=@_err;
+ nek_analyze _name "-f" "1" "-e" "9" stdout=@_out stderr=@_err;
 }
 
 app
--- nek.swift
+++ nek.swift
@@ -5,6 +5,7 @@ type file;
 file json <"LST.json">;
 file tusr <"LST_f90.tusr">;
 float[] viscosities=[0.001, 0.002];
+//float[] viscosities=[0.001, 0.002, 0.003, 0.004, 0.005, 0.006];
 
 
 foreach viscosity,i in viscosities {
@@ -17,17 +18,27 @@ foreach viscosity,i in viscosities {
   (runconfig) = app_make_config(json, viscosity );
 
   /* Construct input files and build the nek5000 executable */
-  string name=sprintf("RTI_LST-%f", viscosity);
-  file RTIjson <single_file_mapper; file=sprintf("%s/%s.json", tdir, name)>;
-  file rea     <single_file_mapper; file=sprintf("%s/%s.rea",  tdir, name)>;
-  file map     <single_file_mapper; file=sprintf("%s/%s.map",  tdir, name)>;
+  string name=sprintf("./RTI_LST-%f", viscosity);
+  //file RTIjson  <single_file_mapper; file=sprintf("%s/%s.json", tdir, name)>;
+  file RTIjson  <simple_mapper; location=tdir, prefix=name, suffix=".json">;
+  file rea      <single_file_mapper; file=sprintf("%s/%s.rea",  tdir, name)>;
+  file map      <single_file_mapper; file=sprintf("%s/%s.map",  tdir, name)>;
+  file usr      <single_file_mapper; file=sprintf("%s/%s.usr",  tdir, name)>;
+  file size_mod <single_file_mapper; file=sprintf("%s/size_mod.F90",  tdir, name)>;
+
+  (usr, rea, map, RTIjson, size_mod) = app_genrun (runconfig, tusr, "/home/maxhutch/simple/nek/makenek", name, tdir);
+  
   file nek5000 <single_file_mapper; file=sprintf("%s/nek5000", tdir, name)>;
-  (nek5000, rea, map, RTIjson) = app_genrun (runconfig, tusr, "@pwd/nek/makenek", name, tdir);
+  (nek5000) = app_makenek(tdir, name, usr, size_mod);
 
   /* Run Nek! */
   file donek_o <single_file_mapper; file=sprintf("%s/donek_out.txt", tdir)>;
   file donek_e <single_file_mapper; file=sprintf("%s/donek_err.txt", tdir)>;
-  file[] RTIfiles <simple_mapper; location=tdir, prefix=sprintf("%s0.f", name), padding=5>;
+  string[auto] RTI_outfile_names;
+  foreach j in [1:9]{
+    RTI_outfile_names << sprintf("%s/%s0.f0000%i", tdir, name, j);
+  }
+  file[] RTIfiles <array_mapper; files=RTI_outfile_names>;
   (donek_o, donek_e, RTIfiles) = app_donek(rea, map, tdir, name, nek5000);
 
   /* Analyze the outputs, making a bunch of pngs */
