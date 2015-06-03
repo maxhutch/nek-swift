eef47baf7358c72822d583779d5c9ad98927fa20
Max Hutchinson <maxhutch@gmail.com>
2015-06-03 14:59:04 +0000

    Attempt to add iteration.  Stalls when it enters the iterate block.

--- apps.swift
+++ apps.swift
@@ -27,6 +27,15 @@ genrun
   }
 }
 
+app 
+(file _config)
+app_gensub
+(file _json, file _tusr, string _name, string _tdir, string _pname, float _pval)
+{
+  genrun "-d" @_json "-u" @_tusr "--tdir" _tdir _name "--legacy" "--no-make" strcat("--override={\"",_pname,"\": ", _pval, "}");
+}
+
+
 app
 (file _nek5000)
 app_makenek_new
@@ -56,9 +65,17 @@ makenek
 
 
 app
-(file _out, file _err, file[] _RTIfiles)
+(file _out, file _err, file[] _RTIfiles, file[] _checkpoint)
 app_donek
 (file _rea, file _map, string _tdir, string _name, file _nek5000)
+{
+ nekmpi _name "256" "32" _tdir;
+}
+
+app
+(file _out, file _err, file[] _RTIfiles, file[] _checkpoint)
+app_donek_restart
+(file _rea, file _map, string _tdir, string _name, file _nek5000, file[] _inpoint)
 {
  nekmpi _name "256" "32" _tdir;
 }
@@ -67,34 +84,37 @@ app_donek
 app
 (file _out, file _err, file[] _pngs)
 app_nek_analyze
-(file _RTIjson, file[] _RTIfiles, string _name)
+(file _RTIjson, file[] _RTIfiles, file[] _checkpoints, string _name, int _start, int _end)
 {
- nek_analyze _name "-f" "1" "-e" "5" "-nt" "16" "-nb" "256" stdout=@_out stderr=@_err;
+ nek_analyze _name "-f" _start "-e" _end "-nt" "16" "-nb" "256" stdout=@_out stderr=@_err;
 }
 
 
-(string[auto] _ofs) nek_out_names(string _tdir, string _name, int _nout, int _nwrite)
+(string[auto] _checkpoint, string[auto] _ofs) nek_out_names(string _tdir, string _name, int _nstart, int _nend, int _nwrite)
 {
-  foreach i in [1:_nout]{
+  foreach i in [_nstart:_nend-1]{
     foreach j in [0:_nwrite-1]{
       _ofs << sprintf("%s/A%s/%s%s.f%s", _tdir, pad(2, j), _name, pad(2,j), pad(5, i));
     }
+  }
+  foreach j in [0:_nwrite-1]{
+    _ofs << sprintf("%s/A%s/%s%s.f%s", _tdir, pad(2, j), _name, pad(2,j), pad(5, _nend));
   }
 }
 
 app
 (file _out, file _err)
 app_archive
-(string _name, file[] _raw)
+(string _name, file[] _raw, file[] _checkpoint, int _start, int _end)
 {
-  post_proc strcat("nek-swift/",_name) "-f" "1" "-e" "5" "--archive" "--no-process" "--no-sync" "--no-upload" stdout=@_out stderr=@_err;
+  post_proc strcat("nek-swift/",_name) "-f" _start "-e" _end "--archive" "--no-process" "--no-sync" "--no-upload" stdout=@_out stderr=@_err;
 }
 
 app
 (file _out, file _err)
 app_upload
-(string _name, file _config, file[] _chest, file[] _img)
+(string _name, file _config, file[] _chest, file[] _img, int _start, int _end)
 {
-  post_proc strcat("nek-swift/",_name) "-f" "1" "-e" "5" "--no-archive" "--no-process" "--no-sync" "--upload" stdout=@_out stderr=@_err;
+  post_proc strcat("nek-swift/",_name) "-f" _start "-e" _end "--no-archive" "--no-process" "--no-sync" "--upload" stdout=@_out stderr=@_err;
 }
 
--- nek.swift
+++ nek.swift
@@ -1,6 +1,10 @@
 import "apps";
 import "Hill";
 
+int nstep = 100;
+int io_step = 25;
+float step_block = 50.0;
+
 foreach pval,i in pvals {
 
   /* Pick a directory to run in */
@@ -8,43 +12,85 @@ foreach pval,i in pvals {
   string name = sprintf("./%s_%s_%f", prefix, pname, pval);
 
   /* Construct input files and build the nek5000 executable */
-  file config   <single_file_mapper; file=sprintf("%s/%s.json", tdir, name)>;
+  file base     <single_file_mapper; file=sprintf("%s/%s.json", tdir, name)>;
   file rea      <single_file_mapper; file=sprintf("%s/%s.rea",  tdir, name)>;
   file map      <single_file_mapper; file=sprintf("%s/%s.map",  tdir, name)>;
   file usr      <single_file_mapper; file=sprintf("%s/%s.usr",  tdir, name)>;
   //file size_mod <single_file_mapper; file=sprintf("%s/SIZE",  tdir, name)>;
   file size_mod <single_file_mapper; file=sprintf("%s/size_mod.F90",  tdir, name)>;
 
-  (usr, rea, map, config, size_mod) = genrun (json, tusr, name, tdir, pname, pval, _legacy=legacy);
+  (usr, rea, map, base, size_mod) = genrun (json, tusr, name, tdir, pname, pval, _legacy=legacy);
   
   file nek5000 <single_file_mapper; file=sprintf("%s/nek5000", tdir, name)>;
   (nek5000) = makenek(tdir, name, usr, size_mod, _legacy=legacy);
 
-  /* Run Nek! */
-  file donek_o <single_file_mapper; file=sprintf("%s/%s.output", tdir, name)>;
-  file donek_e <single_file_mapper; file=sprintf("%s/%s.error", tdir, name)>;
-  string[auto] outfile_names;
-  outfile_names = nek_out_names(tdir, name, nout, nwrite);
-  file[] outfiles <array_mapper; files=outfile_names>;
+  float[int] iout;
+  iout[0] = 0.0;
+  int[int] istep;
+  istep[0] = 0;
+  iterate j {
+    string name_j = sprintf("./%s_%s_%f-%d", prefix, pname, pval, j);
+    string name_tj = sprintf("./%s_%s_%f-t%d", prefix, pname, pval, j);
+    file config     <single_file_mapper; file=sprintf("%s/%s-%d.json", tdir, name, j)>;
+    file tconf      <single_file_mapper; file=sprintf("%s/%s-t%d.json", tdir, name, j)>;
+    file rea_j      <single_file_mapper; file=sprintf("%s/%s-%d.rea",  tdir, name, j)>;
+    file map_j      <single_file_mapper; file=sprintf("%s/%s-%d.map",  tdir, name, j)>;
+    file usr_j      <single_file_mapper; file=sprintf("%s/%s-%d.usr",  tdir, name, j)>;
+    //file size_mod <single_file_mapper; file=sprintf("%s/SIZE",  tdir, name)>;
+    file size_mod_j <single_file_mapper; file=sprintf("%s/size_mod-%d.F90",  tdir, name, j)>;
 
-  (donek_o, donek_e, outfiles) = app_donek(rea, map, tdir, name, nek5000);
+    (tconf) = app_gensub (base, tusr, name_tj, tdir, "num_steps", step_block);
+    float iout_l;
+    if (iout[j] < 1.1) {
+      iout_l = 0.0;
+    } else { 
+      iout_l = iout[j];
+    }
 
-  /* Analyze the outputs, making a bunch of pngs */
-  file analyze_o <single_file_mapper; file=sprintf("%s/analyze_out.txt", tdir)>;
-  file analyze_e <single_file_mapper; file=sprintf("%s/analyze_err.txt", tdir)>;
-  file[] pngs <filesys_mapper; pattern=sprintf("%s/%s*.png", tdir, name)>;
-  //file[] chest<filesys_mapper; pattern=sprintf("%s/%s-results/*", tdir, name)>;
-  (analyze_o, analyze_e, pngs) = app_nek_analyze(config, outfiles, sprintf("%s/%s",tdir,name));
-  //(analyze_o, analyze_e, chest, pngs) = app_nek_analyze(config, outfiles, sprintf("%s/%s",tdir,name));
+    (usr_j, rea_j, map_j, config, size_mod_j) = genrun (tconf,  tusr, name_j, tdir, "restart",   iout_l, _legacy=legacy);
 
-  /* Archive the outputs to HPSS */
-  file arch_o <single_file_mapper; file=sprintf("%s/arch.output", tdir, name)>;
-  file arch_e <single_file_mapper; file=sprintf("%s/arch.error", tdir, name)>;
-  (arch_o, arch_e) = app_archive(sprintf("%s/%s", tdir, name), outfiles);
+    /* Run Nek! */
+    file donek_o <single_file_mapper; file=sprintf("%s/%s-%d.output", tdir, name, j)>;
+    file donek_e <single_file_mapper; file=sprintf("%s/%s-%d.error", tdir, name, j)>;
+    string[auto] outfile_names_j;
+    string[auto] checkpoint_names_j;
+    (checkpoint_names_j, outfile_names_j) = nek_out_names(tdir, name_j, toInt(toString(iout[j])), toInt(toString(iout[j] + step_block / io_step)), nwrite);
+    file[] outfiles_j <array_mapper; files=outfile_names_j>;
+    file[] checkpoints_j <array_mapper; files=checkpoint_names_j>;
+    if (j == 0){
+      (donek_o, donek_e, outfiles_j, checkpoints_j) = app_donek(rea_j, map_j, tdir, name_j, nek5000);
+    } else {
+      string[auto] old_checkpoints;
+      string[auto] old_outputs;
+      (old_checkpoints, old_outputs) = nek_out_names(tdir, sprintf("./%s_%s_%f-%d", prefix, pname, pval, j), toInt(toString(iout[j-1])), toInt(toString(iout[j] - 1)), nwrite);
+      file[] checks_old <array_mapper; files=old_checkpoints>;
+      file[] checks <structured_regexp_mapper; source=checks_old, 
+        match=sprintf("%s_%s_%f/%s_%s_f-%d(.*)", prefix, pname, pval, prefix, pname, pval, j-1), 
+        transform=sprintf("%s_%s_%f/%s_%s_f-%d\\1", prefix, pname, pval, prefix, pname, pval, j)>;
+      (donek_o, donek_e, outfiles_j, checkpoints_j) = app_donek_restart(rea_j, map_j, tdir, name_j, nek5000, checks);
+    }
+ 
+
+    /* Analyze the outputs, making a bunch of pngs */
+    file analyze_o <single_file_mapper; file=sprintf("%s/analyze-%d_out.txt", tdir, j)>;
+    file analyze_e <single_file_mapper; file=sprintf("%s/analyze-%d_err.txt", tdir, j)>;
+    file[] pngs <filesys_mapper; pattern=sprintf("%s/%s*.png", tdir, name_j)>;
+    //file[] chest<filesys_mapper; pattern=sprintf("%s/%s-results/*", tdir, name)>;
+    (analyze_o, analyze_e, pngs) = app_nek_analyze(config, outfiles_j, checkpoints_j, sprintf("%s/%s",tdir,name), toInt(toString(iout)), toInt(toString(iout[j] + step_block / io_step)) );
+
+    /* Archive the outputs to HPSS */
+    file arch_o <single_file_mapper; file=sprintf("%s/arch-%d.output", tdir, j)>;
+    file arch_e <single_file_mapper; file=sprintf("%s/arch-%d.error", tdir, j)>;
+    (arch_o, arch_e) = app_archive(sprintf("%s/%s", tdir, name_j), outfiles_j, checkpoints_j, toInt(toString(iout[j])), toInt(toString(iout[j] + step_block / io_step)));
+
+   istep[j+1] = istep[j] + toInt(toString(step_block));
+   iout[j+1] = iout[j] + step_block / io_step;
+  } until (istep[j+1] >= nstep);
 
   /* Publish the outputs to Petrel 
   file uplo_o <single_file_mapper; file=sprintf("%s/uplo.output", tdir, name)>;
   file uplo_e <single_file_mapper; file=sprintf("%s/uplo.error", tdir, name)>;
   (uplo_o, uplo_e) = app_upload(sprintf("%s/%s", tdir, name), config, chest, pngs);
   */
+  
 }
