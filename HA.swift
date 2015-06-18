1646b5ce5030374c6cd51fd6a34b6937d08a497d
Max Hutchinson <maxhutch@gmail.com>
2015-06-18 19:16:36 +0000

    Updating input files.

--- HA.swift
+++ HA.swift
@@ -1,20 +1,25 @@
 type file;
 
 /* Inputs: standard dictionary, user file, and list of viscosities */
-string prefix="HA2";
+string prefix="HA";
 file json <"HA_sym.json">;
 file tusr <"single_mode.tusr">;
 //file tusr <"Hill_f77.tusr">;
-string pname="npres";
-//float[] pvals=[4.0, 8.0, 12.0, 16.0];
-float[] pvals=[2.0];
+//string pname="tolrel";
+string pname="visc";
+float[] pvals = [0.0001];
 //float[] pvals=[0.001, 0.002, 0.003, 0.004, 0.005, 0.006];
-int nwrite = 256;
+int nwrite=256;
 boolean legacy = false;
 
-int nstep = 256;
+/*
+int nstep = 16384;
 int io_step = 128;
-int step_block = 256;
-int foo = 2;
-
+int step_block = 1024;
+int foo = 8;
+*/
+int nstep = 16384;
+int io_step = 128;
+int step_block = 512;
+int foo = 4;
 
